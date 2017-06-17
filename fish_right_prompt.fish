b86aa32d8b79bc58038dabf867365c66a92755f0
Jianing Yang <jianingy.yang@gmail.com>
2017-06-17 09:11:42 +0800

    small fix

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -6,11 +6,12 @@ function fish_right_prompt
   set -l normal (set_color normal)
 
   if test -n "$SSH_CONNECTION"
-    set host $cyan (hostname) $grey ":"
-    set underline (set_color -u)
+    set -l host $cyan (hostname) $grey ":"
+    set -l underline (set_color -u)
+    echo -n -s $grey ' ← ' $host $underline (prompt_pwd) $normal
+  else
+    echo -n -s $grey ' ← ' (prompt_pwd) $normal
   end
-
-  echo -n -s $grey ' ← ' $host $underline (prompt_pwd) $normal
 
   if test $last_status -ne 0
     set_color red
