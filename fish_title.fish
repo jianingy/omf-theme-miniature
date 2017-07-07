52dfb1e265900d0e1e5ed04a172400ea602c8c32
Jianing Yang <jianingy.yang@gmail.com>
2017-07-07 10:44:51 +0800

    change title on the fly

--- fish_title.fish
+++ fish_title.fish
@@ -1,3 +1,11 @@
 function fish_title
   # Customize terminal window title
+  hostname
+  if [ -n "$TMUX_PANE" ] 
+    tmux_pane_title
+  end
+end
+
+function tmux_pane_title
+  printf "\033k"(hostname)"\033\\"
 end
